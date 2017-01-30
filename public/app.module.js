9f4829c8bd5ebf41fc21c421848df30c2d2fb44c
Evan Busse <evanbusse@gmail.com>
2017-01-30 13:42:03 -0700

    injected $http, getting data from api

--- public/app.module.js
+++ public/app.module.js
@@ -1,17 +1,26 @@
 (function() {
   'use strict';
+  const DATABASE_URL = 'https://eb-classified-api.herokuapp.com';
 
   angular.module('app', [])
   .component('fluff', {
     controller: controller,
-    template: 'this'
+    templateURL: 'template.html'
   });
 
-  function controller() {
+  controller.$inject = ['$http'];
+  function controller($http) {
     const vm = this;
 
     vm.$onInit = function() {
+      this.getAll()
     }
+
+    vm.getAll = function () {
+      $http.get(`${DATABASE_URL}/classifieds`).then((data) => {
+        console.log(data);
+      });
+    };
   }
 
 }());
