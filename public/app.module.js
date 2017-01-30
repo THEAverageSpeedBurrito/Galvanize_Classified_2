1693b648cb0e8f6ba27584cc4b834a4dcc563d59
Evan Busse <evanbusse@gmail.com>
2017-01-30 14:57:20 -0700

    patch working

--- public/app.module.js
+++ public/app.module.js
@@ -13,11 +13,20 @@
         <input type="text" name="image url" ng-model="$ctrl.post.item_image" placeholder="image url"/>
         <button type="submit" class="btn btn-primary" ng-click="$ctrl.createPost()">Create Post</button>
       </form>
+      <form novalidate name="editform">
+        <input type="text" name="postId" ng-model="$ctrl.update.id" placeholder="post id"/>
+        <input type="text" name="title" ng-model="$ctrl.update.title" placeholder="Title"/>
+        <input type="text" name="price" ng-model="$ctrl.update.price" placeholder="Price"/>
+        <input type="text" name="description" ng-model="$ctrl.update.description" placeholder="Description"/>
+        <input type="text" name="image url" ng-model="$ctrl.update.item_image" placeholder="image url"/>
+        <button type="submit" class="btn btn-primary" ng-click="$ctrl.updatePost()">Update Post</button>
+      </form>
       <div ng-repeat="post in $ctrl.postData">
+        <p>id: {{post.id}}</p>
         <img src={{post.item_image}}/>
         <p>{{post.title}}</p>
         <p>$ {{post.price}}</p>
-        <p>{{post.description}}</p>
+        <p>Description: {{post.description}}</p>
         <hr/>
       </div>
     `
@@ -35,9 +44,19 @@
       $http.post(`http://localhost:3000/classifieds`, vm.post)
       .then((res) => {
         console.log(res);
+        vm.postData.push(res.data)
         delete vm.post;
       })
     };
+
+    vm.updatePost = function () {
+      var id = vm.update.id;
+      delete vm.update.id;
+      $http.patch(`http://localhost:3000/classifieds/${id}`, vm.update)
+      .then((res) => {
+        console.log(res);
+      })
+    }
 
     vm.getAll = function () {
       $http.get(`http://localhost:3000/classifieds`).then((res) => {
